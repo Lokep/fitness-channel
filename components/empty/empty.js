34a67b354a5c3fd4573ef5e10d8e2e90c8918c57
Cieme <aslslgood22@163.com>
2021-09-28 17:33:25 +0800

    运动打卡一部分

---
+++ components/empty/empty.js
@@ -0,0 +1,30 @@
+// components/empty/empty.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    picurl: {
+      type: String,
+      value: "../../assets/images/empty-image-default.png"
+    },
+    title: {
+      type: String,
+      value: "暂无数据"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
