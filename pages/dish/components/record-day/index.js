3cde08fb39f281f3010c1686af2f69cbe6817ee3
Lokep <iamlokep@163.com>
2021-10-08 16:19:16 +0800

    feat: 联调接口

--- pages/dish/components/record-day/index.js
+++ pages/dish/components/record-day/index.js
@@ -1,4 +1,9 @@
-// pages/dish/components/record-day/index.js
+import dayjs from "dayjs";
+import { getClockRecordByDate } from "../../../../api/dish";
+import { getCache } from "../../../../utils/util";
+
+const MONTH_TIME_STAMP = 30 * 24 * 3600 * 1000;
+
 Component({
   options: {
     styleIsolation: "shared",
@@ -7,8 +12,16 @@ Component({
   properties: {},
 
   data: {
-    mealType: 0,
+    mealType: "1",
     dietPlan: {},
+    show: false,
+    date: handleDate(new Date()),
+    minDate: new Date(2020, 8, 17).getTime(),
+    maxDate: new Date().getTime() + MONTH_TIME_STAMP,
+  },
+
+  attached() {
+    this.getClockRecordByDate();
   },
 
   methods: {
@@ -17,5 +30,51 @@ Component({
         mealType: e.detail.name,
       });
     },
+
+    showCalendar() {
+      this.setData({
+        show: true,
+      });
+    },
+
+    onClose() {
+      this.setData({
+        show: false,
+      });
+    },
+    onConfirm(e) {
+      console.log(e);
+      this.setData({
+        date: handleDate(e.detail),
+      });
+      this.onClose();
+    },
+
+    getClockRecordByDate() {
+      const { mealType, date } = this.data;
+      const { memberId } = getCache("loginInfo");
+      getClockRecordByDate({
+        memberId,
+        mealType,
+        date: handleTime(date),
+      }).then((res) => {
+        if (res.result === 1) {
+        }
+      });
+    },
+  },
+
+  observers: {
+    date(v) {
+      this.getClockRecordByDate();
+    },
   },
 });
+
+function handleTime(date) {
+  return dayjs(date).format("YYYY-MM-DD hh:mm:ss");
+}
+
+function handleDate(date) {
+  return dayjs(date).format("YYYY-MM-DD");
+}
